4aa272bb9dfd4b5c8fd221e1fa0761a8cc03d0e9
sumana10 <sumanaonlyme@gmail.com>
2021-12-15 15:55:16 +0530

    first-commit

---
+++ src/components/Todo.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ListGroup, ListGroupItem } from 'reactstrap'
+import {FaCheckDouble} from "react-icons/fa"
+
+import { removeTodo } from '../action/todo'
+import {connect} from 'react-redux'
+
+const Todo = ({todos, markComplete}) => {
+  return (
+    <ListGroup className='mt-5 mb-2 items'>
+    {todos.map((todo) =>(
+      <ListGroupItem key={todo.id}>
+        {todo.title}
+        <span onClick={() =>markComplete(todo.id)} className='float-end'>
+          <FaCheckDouble/>
+        </span>
+      </ListGroupItem>
+    ))}
+    </ListGroup>
+  )
+}
+const mapStateToProps = state =>({
+  todos: state.todos
+})
+
+const mapDispatchToProps = dispatch =>({
+  markComplete: id =>{
+    dispatch(removeTodo(id))
+  }
+})
+export default connect(mapStateToProps, mapDispatchToProps)(Todo)
---
+++ src/components/TodoForm.js
@@ -0,0 +1,67 @@
+import React,{ useState } from 'react'
+
+import { 
+ Form,
+ FormGroup,
+ Input,
+ Button,
+ InputGroup,
+ InputGroupAddon
+} from 'reactstrap'
+
+import {v4} from 'uuid'
+
+import {connect} from 'react-redux'
+
+import {addTodo} from  '../action/todo'
+
+ const TodoForm = ({addTodo}) => {
+
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+    if(title === ''){
+      return alert('Please add a todo')
+    }
+
+    const todo = {
+      title,
+      id: v4()
+    }
+    addTodo(todo);
+    setTitle("");
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <InputGroup>
+          <Input
+            type="text"
+            name="todo"
+            id="todo"
+            placeholder='Your next Todo'
+            value={title}
+            onChange={e =>setTitle(e.target.value)}
+          />
+          <Button color="info" onClick={handleSubmit}>ADD</Button>
+         </InputGroup>
+      </FormGroup>
+    </Form>
+  )
+}
+
+const mapStateToProps = state => ({
+ //to bring data from the state into component
+ //from the central state into your application
+})
+
+const mapDispatchToProps = dispatch =>({
+
+  addTodo: todo =>{
+    dispatch(addTodo(todo))
+  }
+
+})
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
