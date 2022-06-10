1c17ad1e15a5f941ea25e71a6bb0e6da0c7671b5
sumana10 <sumanaonlyme@gmail.com>
2022-06-10 12:21:00 +0530

    changes in page style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import react from 'react'
-import {Container} from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import react from "react";
+import { Container } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import Todo from './components/Todo'
-import TodoForm from './components/TodoForm'
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
+import logo from './logo.svg';
 
-import {Provider} from 'react-redux'
+import { Provider } from "react-redux";
 
-import store from "./store"
+import store from "./store";
 
 function App() {
-//provider makes available store to components
-  return (
-    <Provider store={store}>
-      <Container fluid>
-        <TodoForm/>
-        <Todo/>
-      </Container>
-    </Provider>
-   
-  );
+	//provider makes available store to components
+	return (
+		<Provider store={store}>
+			<Container fluid>
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1>Todo with local storage</h1>
+				<TodoForm />
+				<Todo />
+			</Container>
+		</Provider>
+	);
 }
 
 export default App;
